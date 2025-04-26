21cb89cc5337f2b5531c972db935550a5ead1833
João Paulo Akira Sigue <joaopsigue7@gmail.com>
2025-04-26 20:24:13 -0300

    feat: Created PrecificationType entity

---
+++ server/src/api/precification-type/routes/precification-type.ts
@@ -0,0 +1,7 @@
+/**
+ * precification-type router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::precification-type.precification-type');
---
+++ server/src/api/precification-type/services/precification-type.ts
@@ -0,0 +1,7 @@
+/**
+ * precification-type service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::precification-type.precification-type');
