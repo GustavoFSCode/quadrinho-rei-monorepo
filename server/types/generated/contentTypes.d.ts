e971662883afa86191ded81bb056a4b63772595c
João Paulo Akira Sigue <joaopsigue7@gmail.com>
2025-08-04 23:29:11 -0300

    feat: Created date attribute in Purchase entity

--- server/types/generated/contentTypes.d.ts
+++ server/types/generated/contentTypes.d.ts
@@ -957,6 +957,7 @@ export interface ApiPurchasePurchase extends Struct.CollectionTypeSchema {
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
+    date: Schema.Attribute.Date;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<
       'oneToMany',
